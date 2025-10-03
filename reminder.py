296c41a2b60e67a8a8d05f3b50402d392b2dc8e5
alyalya2013-dev <alyalya2013@gmail.com>
2025-10-03 22:03:45 +0500

    добавлена функция включения музыки

--- reminder.py
+++ reminder.py
@@ -35,6 +35,12 @@ def check():
     window.after(10000, check)
 
 
+def play_snd():
+    pygame.mixer.init()
+    pygame.mixer.music.load('reminder.mp3')
+    pygame.mixer.music.play()
+
+
 window = Tk()
 window.title('Напоминание')
 label = Label(text='Установите напоминание')
